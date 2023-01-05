6d2b5dc6f19775b1d22c61d1b75e14e4f9162388
matthew-mar <mmary.prog@gmail.com>
2023-01-05 18:46:46 +0300

    orders-service / remove from cart

--- app/book_market/orders_service/exceptions/mappers.py
+++ app/book_market/orders_service/exceptions/mappers.py
@@ -3,6 +3,12 @@ class BooksetMapperException(Exception):
 
     BOOK_ALREARY_IN_SET_MESSAGE = "book already in set"
 
+    BOOK_NOT_EXIST_MESSAGE = "book not exist in set"
+
 
 class CartMapperException(Exception):
     BOOK_ALREADY_IN_CART_MESSAGE = "book already in cart"
+
+    BOOK_NOT_EXIST_IN_CART_MESSAGE = "book not exist in cart"
+
+    CART_EMPTY_MESSAGE = "cart is empty"
