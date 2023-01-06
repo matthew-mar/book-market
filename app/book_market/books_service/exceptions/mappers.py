af90baed984736e7ec4d173155b0c8a51d7a088b
matthew-mar <mmary.prog@gmail.com>
2023-01-06 22:55:06 +0300

    book-service / book detail

---
+++ app/book_market/books_service/exceptions/mappers.py
@@ -0,0 +1,2 @@
+class BookMapperException(Exception):
+    BOOK_NOT_EXIST_MESSAGE = "book not exist"
--- app/book_market/books_service/urls.py
+++ app/book_market/books_service/urls.py
@@ -5,4 +5,5 @@ NAMESPACE = "books_service.routes"
 urlpatterns = [
     path("", include(f"{NAMESPACE}.genre")),
     path("", include(f"{NAMESPACE}.author")),
+    path("", include(f"{NAMESPACE}.book")),
 ]
